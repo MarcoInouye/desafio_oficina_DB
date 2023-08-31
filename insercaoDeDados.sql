2ca0512efeb06b955f7370d940d588c178c4f75d
MarcoInouye <139030808+MarcoInouye@users.noreply.github.com>
2023-08-31 09:42:10 -0300

    Update insercaoDeDados.sql

--- insercaoDeDados.sql
+++ insercaoDeDados.sql
@@ -1,107 +1,124 @@
+use oficina_db;
 
-use ecommerce;
-show tables;
--- idClient, Fname, Minit, Lname, CPF, Address
-insert into Clients (Fname, Minit, Lname, CPF, Address) 
-	   values('Mariazinha',Z,Silveira, 12346789, 'rua silva 29, Bom Clima - Guarulhos
-		   (Marco,'A',Inouye, 987644321,'rua alameda 289, Centro - Cidade das flores'),
-			 (Giovanna,'G',Dias, 45678913,'avenida alemeda vinha 1009, Centro - São Paulo'),
-			 ('Julia','V',Pereira, 789123456,'rua lareijras 861, Centro - Cidade das flores'),
-		
+		insert into employee (employeeName, employeeFunction )
+	         values	('Marcelo Maneva','Gerente'),
+			('Ayrton Souza Senna','Mecanico'),
+                        ('Luis Amilton','Pintor'),
+                        ('Pedro Pascal','Funileiro'),
+                        ('Roberto De Niro','Mecanico'),
+                        ('Antonio Sanita','Eletricista'),
+                        ('José Pinto','Auxiliar'),
+                        ('Francisco Mendez','Auxiliar'),
+                        ('Regina Macedo','Recepcionista'),
+                        ('Renato Souza','Supervisor');
+                                
+		select * from employee;
 
--- idProduct, Pname, classification_kids boolean, category('Eletrônico','Vestimenta','Brinquedos','Alimentos','Móveis'), avaliação, size
-insert into product (Pname, classification_kids, category, avaliação, size) values
-							  ('Fone de ouvido',false,'Eletrônico','4',null),
-                              ('Hot Wheels',true,'Brinquedos','3',null),
-                              ('Moletom Lightning Mcqueen',true,'Vestimenta','5',null),
-                              ('Microfone Razer - Youtuber',False,'Eletrônico','4',null),
-                              ('Sofá retrátil',False,'Móveis','3','3x57x80'),
-                              ('Farinha de mandioca',False,'Alimentos','2',null),
-                              ('Fire Stick Amazon',False,'Eletrônico','3',null);
-
-select * from clients;
-select * from product;
--- idOrder, idOrderClient, orderStatus, orderDescription, sendValue, paymentCash
-
-delete from orders where idOrderClient in  (1,2,3,4);
-insert into orders (idOrderClient, orderStatus, orderDescription, sendValue, paymentCash) values 
-							 (1, default,'compra via aplicativo',null,1),
-                             (2,default,'compra via aplicativo',50,0),
-                             (3,'Confirmado',null,null,1),
-                             (4,default,'compra via website',150,0);
-
--- idPOproduct, idPOorder, poQuantity, poStatus
-select * from orders;
-insert into productOrder (idPOproduct, idPOorder, poQuantity, poStatus) values
-						 (1,5,2,null),
-                         (2,5,1,null),
-                         (3,7,1,null);
-
--- storageLocation,quantity
-insert into productStorage (storageLocation,quantity) values 
-							('Amazonas',1000),
-                            ('Rio de Janeiro',500),
-                            ('São Paulo',10),
-                            ('São Paulo',100),
-                            ('São Paulo',10),
-                            ('Brasília',60);
-
--- idLproduct, idLstorage, location
-insert into storageLocation (idLproduct, idLstorage, location) values
-						 (1,2,'RJ'),
-                         (2,6,'GO');
-
--- idSupplier, SocialName, CNPJ, contact
-insert into supplier (SocialName, CNPJ, contact) values 
-							('Almeida e filhos', 123456789123456,'21985474'),
-                            ('Eletrônicos Ricardo',854519649143457,'21985484'),
-                            ('Eletrônicos tiozimho do gas', 934567893934695,'21975474');
+		insert into Service (descriptionService, costService, hoursService, categoryService)
+		 values	('Troca da correia dentada', 850, 3, 'Mecanica'),
+			('Troca de óleo do motor', 120, 1, 'Mecanica'),
+                            ('Troca da Embreagem', 250, 2,'Mecanica'),
+                            ('Troca de pneus', 50, 1, 'Rodas'),
+                            ('Balanceamento', 50, 1, 'Rodas'),
+                            ('Alinhamento', 120, 1, 'Rodas'),
+                            ('Pintura',	2800, 30, 'Funilaria'),
+                            ('Troca de bateria', 40, 1, 'Eletrica'),
+                            ('Troca de freios',	160, 1,	'Freios'),
+                            ('Troca de amortecedores',			250,			3,				'Suspensao'),
+                            ('Instalação de Alarem',			100,			2,				'Eletrica'),
+                            ('Troca de farois e lanternas',		120,			1,				'Eletrica'),
+                            ('Regulagem do cambio automatico',	800,			2,				'Mecanica'),
+                            ('Polimento',						250,			2,				'Funilaria'),
+                            ('Manutencao do ar condicionado',	200,			1,				'Mecanica');
                             
-select * from supplier;
--- idPsSupplier, idPsProduct, quantity
-insert into productSupplier (idPsSupplier, idPsProduct, quantity) values
-						 (1,1,500),
-                         (1,2,400),
-                         (2,4,633),
-                         (3,3,5),
-                         (2,5,10);
+        select * from Servico;
 
--- idSeller, SocialName, AbstName, CNPJ, CPF, location, contact
-insert into seller (SocialName, AbstName, CNPJ, CPF, location, contact) values 
-						('Tech electronics', null, 123456789456321, null, 'Rio de Janeiro', 219946287),
-					    ('Boutique léguas',null,null,123456783,'Rio de Janeiro', 219567895),
-						('Kids World',null,456789123654485,null,'São Paulo', 1198657484);
-
-select * from seller;
--- idPseller, idPproduct, prodQuantity
-insert into productSeller (idPseller, idPproduct, prodQuantity) values 
-						 (1,6,80),
-                         (2,7,10);
-
-select * from productSeller;
-
-select count(*) from clients;
-select * from clients c, orders o where c.idClient = idOrderClient;
-
-select Fname,Lname, idOrder, orderStatus from clients c, orders o where c.idClient = idOrderClient;
-select concat(Fname,' ',Lname) as Client, idOrder as Request, orderStatus as Status from clients c, orders o where c.idClient = idOrderClient;
-
-insert into orders (idOrderClient, orderStatus, orderDescription, sendValue, paymentCash) values 
-							 (2, default,'compra via aplicativo',null,1);
-                             
-select count(*) from clients c, orders o 
-			where c.idClient = idOrderClient;
-
-select * from orders;
-
--- recuperação de pedido com produto associado
-select * from clients c 
-				inner join orders o ON c.idClient = o.idOrderClient
-                inner join productOrder p on p.idPOorder = o.idOrder
-		group by idClient; 
+-- [03] inserindo dados para tabela Peca
+		insert into Peca 	(NomePeca, 							CustoPeca)
+				values		('Correia Dentada',					120),
+							('Rolamento correia dentada',		80),
+							('Arruela de vedacao do carter', 	10),
+							('Kit de embreagem',				400),
+							('Pneu',							320),
+							('Tinta de fundo preparador',		120),
+							('Tinta de acabamento',				250),
+							('Amortecedor',						140),
+							('Kit de alarme positronico',		360),
+							('Farol', 							800),
+							('Lanterna dianteira',				220),
+							('Lanterna trazeira',				310),
+							('Correia de ar condicionado',		75),
+                            ('Bateria',							590),
+                            ('Porca para rodas',				20),
+                            ('Arruelas para rodas', 			10);
+                
+		select * from Peca;
         
--- Recuperar quantos pedidos foram realizados pelos clientes?
-select c.idClient, Fname, count(*) as Number_of_orders from clients c 
-				inner join orders o ON c.idClient = o.idOrderClient
-		group by idClient; 
+-- [04] inserindo dados para tabela Produto
+		insert into Produto	(NomeProduto, 				CustoProduto)
+					values	('Desengripante (spray)',	25),
+							('Oleo de motor',			45),
+							('Graxa branca',			85),
+							('Graxa preta',				65),
+							('Lixa 5000',				20),
+							('Lixa 2000',				15),
+							('Lixa 500',				10),
+							('Pasta polidora',			80),
+							('Anti-corrosivo',			50),
+							('Vaselina pastosa',		25),
+							('Oleo de freio',			25),
+							('Oleo de cambio',			80),
+							('Gas de arcondicionado',	200),
+							('Kit de juntas', 			60),
+							('Kit de retentores',		90);
+        
+		select * from Produto;
 
+-- [05] inserindo dados para tabela Veiculo
+		insert into Veiculo (	PlacaVeiculo, 	MarcaVeiculo, 	ModeloVeiculo, 	AnoVeiculo, 	MotorizacaoVeiculo, 	CorVeiculo, 	QuilometragemVeiculo, 	NomeResponsavel, 	TelefoneResponsavel)
+					values	(	'JED8146', 		'Toyota', 		'Corola', 		'2020',			'1.6 FLEX',				'prata',		28467,					'Paulo Fernandes',	'11972587452'),
+							(	'SLO6402', 		'Nissan', 		'Tida', 		'2015',			'1.4 GAS',				'branco',		220670,					'Justino Barbosa',	'11962587431'),
+							(	'LXF0267', 		'Renault', 		'Logan', 		'2011',			'1.6 FLEX',				'preto',		310000,					'Carlos Andrade',	'12952587442'),
+							(	'CVN4608', 		'Hyundai', 		'HB20', 		'2017',			'1.3 FLEX',				'vermelho',		90370,					'Hélio Nunes',		'13942585553'),
+							(	'BNX3079', 		'Chevrolet', 	'Chevette', 	'1978',			'1.4 GAS',				'verde',		430710,					'Benedito Silva',	'13932566454'),
+							(	'LWO4031', 		'Volkswagen',	'Gol',	 		'1992',			'1.8 GAS',				'azul',			303570,					'Hélio Abrantes',	'12938277455');                                
+
+		select * from Veiculo;
+        
+-- [06] inserindo dados para tabela Orcamento
+		insert into Orcamento 	(	idResponsavel_fk, 	idVeiculo_fk, 	Descricao, 										DataInicio,		Valor,	Aprova,	DataTermino,	FormaPagamento)
+					values 		(	11,					1,				'Instalar pneus novos (4 unidades)',			'2023-08-10',	1800,	'S',	'2023-08-10',	default),
+								(	11,					2,				'Instalar farois novos (2 unidades)',			'2023-08-11',	1600,	'N',	NULL,			NULL),
+								(	11,					3,				'Trocar oleo do motor',							'2023-08-11',	180,	'S',	'2023-08-11',	'PIX'),
+								(	11,					4,				'Corrigir falhas do cambio automatico',			'2023-08-12',	960,	'S',	'2023-08-13',	default),
+								(	11,					5,				'Substituir a embreagem',						'2023-08-12',	675,	'S',	'2023-08-14',	default),
+								(	11,					6,				'Substituir a bateria',							'2023-08-12',	680,	'S',	'2023-08-14',	default),
+								(	11,					1,				'Polimento geral da pintura',					'2023-08-15',	330,	NULL,	NULL,			default),
+								(	11,					5,				'Instalacao do sistema de alarme anti-furto',	'2023-08-16',	485,	'S',	'2023-08-17',	default),
+								(	11,					6,				'Substituicao dos amortecedores',				'2023-08-16',	595,	'N',	NULL,			NULL);
+                                
+		select * from Orcamento;
+        
+-- [07] inserindo dados para tabela OrdemServico
+		insert into OrdemServico (	idOrcamento_fk,	idResponsavelOS_fk,	idServico_fk,	idPeca_fk,	idProduto_fk,	Custo)
+						values	(	1,				7,					4,				5,			NULL,			1480),
+								(	1,				8,					5,				15,			NULL,			200),
+                                (	1,				5,					6,				16,			NULL,			120),
+                                
+								(	2,				6,					12,				10,			NULL,			1600),
+                                
+                                (	3,				2,					2,				3,			2,				180),
+                                
+                                (	4,				2,					13,				NULL,		12,				960),
+                                
+                                (	5,				2,					3,				4,			1,				675),
+                                
+                                (	6,				6,					8,				14,			9,				680),
+                                
+                                (	7,				3,					14,				NULL,		8,				330),
+                                
+                                (	8,				6,					11,				9,			10,				485),
+                                
+                                (	9,				5,					10,				8,			4,				595);
+                                
+        select * from OrdemServico;
